52b7ece148d9070ecd72572b7f5408f8618f3ac1
VAN\凡 <103703018@nccu.edu.tw>
2017-06-08 11:48:32 +0800

    新增搜尋  修改sidebar

--- Jooyin/routes/maingroup.js
+++ Jooyin/routes/maingroup.js
@@ -27,11 +27,11 @@ router.get('/', function(req, res, next) {
 		}
 		datalength = rows.length;
 		data = rows;
-		data.splice(0, (page - 1) * 4);
-		data.splice(4, Number.MAX_VALUE);
+		data.splice(0, (page - 1) * 6);
+		data.splice(6, Number.MAX_VALUE);
 
 		
-		db.query('SELECT * FROM group_with_user, agroup WHERE group_with_user.user_name = ? and group_with_user.group_id = agroup.id', res.locals.nickname, function(err, rows) {
+		db.query('SELECT * FROM group_with_user, agroup WHERE group_with_user.user_name = ? and group_with_user.group_id = agroup.id ORDER By id desc', res.locals.nickname, function(err, rows) {
 
 			if (err) {
 				console.log(err);
@@ -39,7 +39,7 @@ router.get('/', function(req, res, next) {
 			myGroup_data = rows;
 
 			//console.log(myGroup_data);
-
+			myGroup_data.splice(3, Number.MAX_VALUE);
 
 			res.render('maingroup', {
 				data : data,
@@ -55,56 +55,57 @@ router.get('/', function(req, res, next) {
 
 });
 
-//router.get('/search', function(req, res, next) {
-//	
-//	res.locals.nickname = req.session.nickname;
-//	res.locals.logined = req.session.logined ;
-//	
-//	var page = 1;
-//
-//	if (req.query.page) {
-//		page = req.query.page;
-//	}
-//	
-//	var db = pool;
-//	var data = "";
-//	var datalength;
-//	var myGroup_data = "";
-//
-//	db.query('SELECT * FROM agroup ORDER By id desc', function(err, rows) {
-//
-//		if (err) {
-//			console.log(err);
-//		}
-//		datalength = rows.length;
-//		data = rows;
-//		data.splice(0, (page - 1) * 4);
-//		data.splice(4, Number.MAX_VALUE);
-//
-//		
-//		db.query('SELECT * FROM group_with_user, agroup WHERE group_with_user.user_name = ? and group_with_user.group_id = agroup.id', res.locals.nickname, function(err, rows) {
-//
-//			if (err) {
-//				console.log(err);
-//			}
-//			myGroup_data = rows;
-//
-//			//console.log(myGroup_data);
-//
-//
-//			res.render('maingroup', {
-//				data : data,
-//				datalength : datalength,
-//				page : page,
-//				myGroup_data : myGroup_data
-//			});
-//		});
-//	});
-//
-//
-//
-//
-//});
+router.post('/', function(req, res, next) {
+	
+	res.locals.nickname = req.session.nickname;
+	res.locals.logined = req.session.logined ;
+	res.locals.search = req.body.search;
+	var page = 1;
+
+	if (req.query.page) {
+		page = req.query.page;
+	}
+	
+	var db = pool;
+	var data = "";
+	var datalength;
+	var myGroup_data = "";
+	var sql = [ req.body.search, req.body.search];
+	 
+	db.query('SELECT * FROM agroup WHERE group_name = ? OR sports_category = ? ORDER By id desc',sql, function(err, rows) {
+
+		if (err) {
+			console.log(err);
+		}
+		datalength = rows.length;
+		data = rows;
+		//data.splice(0, (page - 1) * 6);
+		//data.splice(6, Number.MAX_VALUE);
+
+		
+		db.query('SELECT * FROM group_with_user, agroup WHERE group_with_user.user_name = ? and group_with_user.group_id = agroup.id ORDER By id desc', res.locals.nickname, function(err, rows) {
+
+			if (err) {
+				console.log(err);
+			}
+			myGroup_data = rows;
+
+			//console.log(myGroup_data);
+			myGroup_data.splice(3, Number.MAX_VALUE);
+
+			res.render('maingroup', {
+				data : data,
+				datalength : datalength,
+				page : page,
+				myGroup_data : myGroup_data
+			});
+		});
+	});
+
+
+
+
+});
 
 
 
